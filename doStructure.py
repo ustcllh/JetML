f14cb59261dba5394e912c25aa744d225930819a
ustcllh <usctllh@gmail.com>
2020-11-02 11:05:23 +0800

    add jet-by-jet cs

--- doStructure.py
+++ doStructure.py
@@ -1,7 +1,5 @@
 #!/opt/conda/bin/python
 
-
-
 ########################################
 #               parsing
 ########################################
@@ -9,25 +7,16 @@ import sys, argparse
 
 parser = argparse.ArgumentParser(description='JetML CML Parser')
 parser.add_argument('-i','--input', help='Input file name', required=True)
+parser.add_argument('-b','--background',help='Background file name', required=True)
 parser.add_argument('-o','--output',help='Output file name', required=True)
-parser.add_argument('-b','--background',help='Background file name', required=False)
 parser.add_argument('-n',help='number of events', required=True)
 
 args = parser.parse_args()
 
-# CS parameters
-max_distance = 0.25
-alpha = 1
-ghost_area = 0.0025
-
-# ICS parameters
-max_distances = [0.2, 0.1]
-alphas = [1, 1]
-
 # print values
 print('Input file: %s' % args.input)
-print('Output file: %s' % args.output)
 print('Background file: %s' % args.background)
+print('Output file: %s' % args.output)
 print('Number of events: %s' % args.n)
 
 
@@ -40,9 +29,10 @@ from array import array
 
 output = TFile(args.output, 'recreate')
 
-tr = TTree('jet', 'jet substructure')
-trcs = TTree('csjet', 'csjet substructure')
-trics = TTree('icsjet', 'icsjet substructure')
+tr = TTree('jet', 'jet')
+trcsj = TTree('csjjet', 'cs jet by jet')
+trcse = TTree('csejet', 'cs event wide')
+trics = TTree('icsjet', 'cs iterative')
 
 nevent_max = int(args.n)
 maxn = 100
@@ -60,7 +50,10 @@ phi = array('f', [0.])
 jetpt = array('f', [0.])
 depth = array('i', [0])
 jetm = array('f', [0.])
+
+# groomed jet variables
 jetmg = array('f', [0.])
+depthg = array('i', [0])
 
 # sequential variables
 z = array('f', maxn * [0.])
@@ -69,18 +62,24 @@ kperp = array('f', maxn * [0.])
 m = array('f', maxn * [0.])
 
 def set_branches(tr):
+    # event variables
     tr.Branch('event', event, 'event/I')
     tr.Branch('x', x, 'x/F')
     tr.Branch('y', y, 'y/F')
     tr.Branch('weight', weight, 'weight/F')
 
+    # jet variables
     tr.Branch('eta', eta, 'eta/F')
     tr.Branch('phi', phi, 'phi/F')
     tr.Branch('jetpt', jetpt, 'jetpt/F')
     tr.Branch('depth', depth, 'depth/I')
     tr.Branch('jetm', jetm, 'jetm/F')
+
+    # groomed jet variables
+    tr.Branch('depthg', depthg, 'depthg/I')
     tr.Branch('jetmg', jetmg, 'jetmg/F')
 
+    # substructure variables without grooming
     tr.Branch('z', z, 'z[depth]/F')
     tr.Branch('delta', delta, 'delta[depth]/F')
     tr.Branch('kperp', kperp, 'kperp[depth]/F')
@@ -88,7 +87,8 @@ def set_branches(tr):
 
 
 set_branches(tr)
-set_branches(trcs)
+set_branches(trcsj)
+set_branches(trcse)
 set_branches(trics)
 
 
@@ -115,6 +115,14 @@ beta=0.
 sd = SoftDropGroomer(zcut=0.1, beta=0.)
 print('Soft Drop Groomer: zcut=%.1f, beta=%.1f' % (zcut, beta))
 
+# CS parameters
+max_distance = 0.25
+alpha = 1
+ghost_area = 0.0025
+
+# ICS parameters
+max_distances = [0.2, 0.1]
+alphas = [1, 1]
 
 # jet clustering w/o background
 def do_clustering(hard_event, ptmin=100.):
@@ -123,9 +131,41 @@ def do_clustering(hard_event, ptmin=100.):
     jets = clust_seq.inclusive_jets(ptmin)
     return jets, clust_seq
 
+# cs jet by jet
+def do_cs_jet_by_jet(full_event, ptmin=100.):
+    # clustering with ghosts and get the jets
+    jet_def = fj.JetDefinition(fj.antikt_algorithm, 0.4)
+    ghost_RapMax = 4.
+    ghost_spec = fj.GhostedAreaSpec(ghost_RapMax, 1, ghost_area)
+    area_def = fj.AreaDefinition(fj.active_area_explicit_ghosts, ghost_spec)
+    clust_seq_full = fj.ClusterSequenceArea(full_event, jet_def, area_def)
+    full_jets = clust_seq_full.inclusive_jets(ptmin)
+
+    # background estimation
+    jet_def_rho= fj.JetDefinition(fj.kt_algorithm, 0.4)
+    area_def_rho = fj.AreaDefinition(fj.active_area_explicit_ghosts, ghost_spec)
+
+    rho_range = fj.SelectorAbsRapMax(4.0)
+    bge_rho = fj.JetMedianBackgroundEstimator(rho_range, jet_def_rho, area_def_rho)
+    bge_rho.set_particles(full_event)
+
+    # subtractor
+    subtractor = cs.ConstituentSubtractor()
+    subtractor.set_distance_type(0)
+    subtractor.set_max_eta(4.0)
+    subtractor.set_background_estimator(bge_rho)
+
+
+    # do subtraction
+    corrected_jets = cs.PseudoJetVec()
+    for jet in full_jets:
+        subtracted_jet = subtractor.result(jet)
+        corrected_jets.push_back(subtracted_jet)
+
+    return corrected_jets, []
+
 # jet clustering with cs
 def do_cs_event_wide(full_event, ptmin=100.):
-    # cuts
     max_eta = 4.
 
     # background estimator
@@ -136,7 +176,7 @@ def do_cs_event_wide(full_event, ptmin=100.):
     #     angle
     # }
     subtractor.set_distance_type(0)
-    # free parameters: max distance (0.25), alpha (1), ghost area (0.0025)
+
     subtractor.set_max_distance(max_distance)
     subtractor.set_alpha(alpha)
     subtractor.set_ghost_area(ghost_area)
@@ -145,9 +185,6 @@ def do_cs_event_wide(full_event, ptmin=100.):
     sel_max_pt = fj.SelectorPtMax(15)
     subtractor.set_particle_selector(sel_max_pt)
     subtractor.initialize()
-
-    # print('Constituent Subtractor')
-    # print(subtractor.description())
 
     bge_rho.set_particles(full_event)
 
@@ -167,12 +204,10 @@ def do_cs_iterative(full_event, ptmin=100.):
     subtractor = ics.IterativeConstituentSubtractor()
     subtractor.set_distance_type(0)
 
-    # max_distances(0.2, 0.1)
     max_distances_vec = ics.DoubleVec()
     max_distances_vec.push_back(max_distances[0])
     max_distances_vec.push_back(max_distances[1])
 
-    # alphas (1, 1)
     alphas_vec = ics.DoubleVec()
     alphas_vec.push_back(alphas[0])
     alphas_vec.push_back(alphas[1])
@@ -188,9 +223,6 @@ def do_cs_iterative(full_event, ptmin=100.):
 
     subtractor.set_particle_selector(sel_max_pt)
     subtractor.initialize()
-
-    # print('Iterative Constituent Subtractor')
-    # print(subtractor.description())
 
     bge_rho.set_particles(full_event)
 
@@ -258,6 +290,7 @@ while dict['0'] and dict_bkg['1'] and nevent<nevent_max:
         y[0] = 0
         weight[0] = 1
         depth[0] = 0
+        depthg[0] = 0
 
         jtr = JetTree(rjet)
         eta[0] = jtr.pseudojet().eta()
@@ -265,21 +298,63 @@ while dict['0'] and dict_bkg['1'] and nevent<nevent_max:
         jetpt[0] = jtr.pseudojet().pt()
         jetm[0] = jtr.pseudojet().m()
 
-        while jtr.has_structure():
-            z[depth[0]] = jtr.z()
-            delta[depth[0]] = jtr.delta()
-            kperp[depth[0]] = jtr.kperp()
-            m[depth[0]] = jtr.m()
+        temp = jtr
+        while temp.has_structure():
+            z[depth[0]] = temp.z()
+            delta[depth[0]] = temp.delta()
+            kperp[depth[0]] = temp.kperp()
+            m[depth[0]] = temp.m()
             depth[0] += 1
-            jtr = jtr.harder()
+            temp = temp.harder()
 
-        jtr.groom(groomer=sd, do_recursive_correction=False)
-        jetmg[0] = jtr.pseudojet().m()
-
+        temp = jtr
+        temp.groom(groomer=sd, do_recursive_correction=True)
+        jetmg[0] = temp.pseudojet().m()
+        while temp.has_structure():
+            depthg[0] += 1
+            temp = temp.harder()
         tr.Fill()
 
+    # cs jet by jet
+    corrected_jets, clust_seq_corr = do_cs_jet_by_jet(full_event, ptmin)
 
-    # jet cs
+    for jet in corrected_jets:
+        if abs(jet.eta())>3:
+            continue
+        constituents = [i for i in jet.constituents()]
+        rjets = jr(constituents)
+        rjet = rjets[0]
+
+        x[0] = 0
+        y[0] = 0
+        weight[0] = 1
+        depth[0] = 0
+        depthg[0] = 0
+
+        jtr = JetTree(rjet)
+        eta[0] = jtr.pseudojet().eta()
+        phi[0] = jtr.pseudojet().phi()
+        jetpt[0] = jtr.pseudojet().pt()
+        jetm[0] = jtr.pseudojet().m()
+
+        temp = jtr
+        while temp.has_structure():
+            z[depth[0]] = temp.z()
+            delta[depth[0]] = temp.delta()
+            kperp[depth[0]] = temp.kperp()
+            m[depth[0]] = temp.m()
+            depth[0] += 1
+            temp = temp.harder()
+
+        temp = jtr
+        temp.groom(groomer=sd, do_recursive_correction=True)
+        jetmg[0] = temp.pseudojet().m()
+        while temp.has_structure():
+            depthg[0] += 1
+            temp = temp.harder()
+        trcsj.Fill()
+
+    # cs event wide
     corrected_jets, clust_seq_corr = do_cs_event_wide(full_event, ptmin)
     for jet in corrected_jets:
         if abs(jet.eta())>3:
@@ -292,6 +367,7 @@ while dict['0'] and dict_bkg['1'] and nevent<nevent_max:
         y[0] = 0
         weight[0] = 1
         depth[0] = 0
+        depthg[0] = 0
 
         jtr = JetTree(rjet)
         eta[0] = jtr.pseudojet().eta()
@@ -299,18 +375,22 @@ while dict['0'] and dict_bkg['1'] and nevent<nevent_max:
         jetpt[0] = jtr.pseudojet().pt()
         jetm[0] = jtr.pseudojet().m()
 
-        while jtr.has_structure():
-            z[depth[0]] = jtr.z()
-            delta[depth[0]] = jtr.delta()
-            kperp[depth[0]] = jtr.kperp()
-            m[depth[0]] = jtr.m()
+        temp = jtr
+        while temp.has_structure():
+            z[depth[0]] = temp.z()
+            delta[depth[0]] = temp.delta()
+            kperp[depth[0]] = temp.kperp()
+            m[depth[0]] = temp.m()
             depth[0] += 1
-            jtr = jtr.harder()
+            temp = temp.harder()
 
-        jtr.groom(groomer=sd, do_recursive_correction=False)
-        jetmg[0] = jtr.pseudojet().m()
-
-        trcs.Fill()
+        temp = jtr
+        temp.groom(groomer=sd, do_recursive_correction=True)
+        jetmg[0] = temp.pseudojet().m()
+        while temp.has_structure():
+            depthg[0] += 1
+            temp = temp.harder()
+        trcse.Fill()
 
     # jet ics
     corrected_jets, clust_seq_corr = do_cs_iterative(full_event, ptmin_cs)
@@ -325,28 +405,34 @@ while dict['0'] and dict_bkg['1'] and nevent<nevent_max:
         y[0] = 0
         weight[0] = 1
         depth[0] = 0
+        depthg[0] = 0
+
         jtr = JetTree(rjet)
         eta[0] = jtr.pseudojet().eta()
         phi[0] = jtr.pseudojet().phi()
         jetpt[0] = jtr.pseudojet().pt()
         jetm[0] = jtr.pseudojet().m()
 
-        while jtr.has_structure():
-            z[depth[0]] = jtr.z()
-            delta[depth[0]] = jtr.delta()
-            kperp[depth[0]] = jtr.kperp()
-            m[depth[0]] = jtr.m()
+        temp = jtr
+        while temp.has_structure():
+            z[depth[0]] = temp.z()
+            delta[depth[0]] = temp.delta()
+            kperp[depth[0]] = temp.kperp()
+            m[depth[0]] = temp.m()
             depth[0] += 1
-            jtr = jtr.harder()
+            temp = temp.harder()
 
-        jtr.groom(groomer=sd, do_recursive_correction=False)
-        jetmg[0] = jtr.pseudojet().m()
+        temp = jtr
+        temp.groom(groomer=sd, do_recursive_correction=True)
+        jetmg[0] = temp.pseudojet().m()
+        while temp.has_structure():
+            depthg[0] += 1
+            temp = temp.harder()
         trics.Fill()
 
     dict, des = rd.next_event()
     dict_bkg, des_dkg = rd_bkg.next_event()
     nevent += 1
-    print(nevent)
     if nevent % 1000 == 0:
         print('%d events completed!' % nevent)
 
