535c645aaf3f3ce9d3fdd1e68893aaf6f074b4b5
LihanLiu <lihan@AS-PHYSL200419S.local>
2021-02-19 10:54:28 -0600

    modify plot macros

--- analysis/plot.py
+++ analysis/plot.py
@@ -1,22 +1,25 @@
-from ROOT import TFile, TTree, TCanvas
-from ROOT import TH1F, TProfile, TLine
-from ROOT import TLegend, TPad, TLatex
+from ROOT import TFile, TTree, TCanvas, TVectorD
+from ROOT import TH1F, TProfile, TF1, TGraphErrors
+from ROOT import TLegend, TLine, TPad, TLatex
 from ROOT import gStyle, gPad
 import sys
 
 gStyle.SetOptStat(1110)
+gStyle.SetOptFit(1001)
 
 # input
 try:
     input = sys.argv[1]
     tree = sys.argv[2]
-    prefix = sys.argv[3]
-    suffix = sys.argv[4]
+    # prefix = sys.argv[3]
+    prefix = tree
+    suffix = sys.argv[3]
 
 except:
     input = './resolution.root'
-    tree = 'jetics'
-    prefix = 'icsjet'
+    tree = 'icsjet'
+    # prefix = 'icsjet'
+    prefix = tree
     suffix = 'default'
 
 f = TFile(input, 'READ')
@@ -51,7 +54,102 @@ def DrawJES():
     c.SaveAs(prefix+'_jes_'+suffix+'.pdf')
 
 def DrawJESDiff():
-    tp = TProfile('tp1', 'JES Profilejet pt', 20, 100, 500)
+    th = []
+    tg_x = []
+    tg_y = []
+    tg_ex = []
+    tg_ey = []
+    tp = TProfile('tp', 'JES Profile;jetpt;JES', 20, 100, 500)
+
+    for i in range(20):
+        title = 'JES (%d<jetpt<%d); JES' % (100+20*i, 120+20*i)
+        th.append(TH1F('', title, 200, 0.8, 1.2))
+        tg_x.append(110+20*i)
+        tg_ex.append(0)
+
+    i=0
+    max = tr.GetEntriesFast()
+    while i<max:
+        tr.GetEntry(i)
+        if tr.depth==0:
+            i+=1
+            continue
+        scale = tr.scale
+        jetpt = tr.jetpt
+        tp.Fill(jetpt, scale, 1)
+        idx = int((jetpt-100)/20)
+        if idx>0 and idx<20:
+            th[idx].Fill(scale, 1)
+        i+=1
+
+    c = TCanvas('c', 'c', 600, 600)
+    gStyle.SetOptStat(11)
+    gStyle.SetOptFit(1001)
+    for i in range(20):
+        tf = TF1('tf', 'gaus', 0.5, 1.5)
+        gaus_max = th[i].GetMaximum()
+        gaus_mean = th[i].GetMean()
+        gaus_dev = th[i].GetRMS()
+        for j in range(10):
+            tf.SetParameters(gaus_max, gaus_mean, gaus_dev)
+            tf.SetRange(gaus_mean-2*gaus_dev, gaus_mean+2*gaus_dev)
+            th[i].Fit('tf', 'QR')
+            gaus_max = tf.GetParameter(0)
+            gaus_mean = tf.GetParameter(1)
+            gaus_dev = tf.GetParameter(2)
+        tg_y.append(tf.GetParameter(1))
+        tg_ey.append(tf.GetParError(1))
+        c.cd()
+        th[i].SetLineColor(4)
+        th[i].SetLineWidth(2)
+        th[i].Draw()
+        tf.Draw("same")
+        tl = TLine(100, 1, 500, 1)
+        tl.SetLineStyle(7)
+        tl.SetLineColor(2)
+        tl.SetLineWidth(2)
+        tl.Draw("same")
+        # c.SaveAs(prefix+'_jes_diff_'+suffix+'%d.pdf'%(i))
+
+    c = TCanvas('c', 'c', 600, 600)
+    c.cd()
+    c.SetBottomMargin(0.1)
+    c.SetLeftMargin(0.15)
+
+    tg = TGraphErrors()
+    for i in range(20):
+        tg.SetPoint(i, tg_x[i], tg_y[i])
+        tg.SetPointError(i, 0, tg_ey[i])
+    tg.SetMaximum(1.24)
+    tg.SetMinimum(0.84)
+    tg.SetLineColor(2)
+    tg.SetLineWidth(2)
+    tg.SetTitle('JES Profile;jetpt;JES')
+    tg.GetYaxis().SetTitleSize(0.14/3)
+    tg.GetYaxis().SetTitleOffset(1.2)
+    tg.GetXaxis().CenterTitle()
+    tg.GetYaxis().CenterTitle()
+    tg.Draw('AP')
+
+    tp.SetLineColor(4)
+    tp.SetLineWidth(2)
+    tp.Draw('same')
+
+    tl = TLine(72, 1, 528, 1)
+    tl.SetLineStyle(7)
+    # tl.SetLineColor(2)
+    tl.SetLineWidth(2)
+    tl.Draw('same')
+
+    lg = TLegend(0.2, 0.7, 0.5, 0.88)
+    lg.AddEntry(tg, 'JES (Peak Fit)', 'le')
+    lg.AddEntry(tp, 'JES (Profile)', 'le')
+    lg.Draw('same')
+
+    c.SaveAs(prefix+'_jes_diff_'+suffix+'.pdf')
+
+def DrawJESProfile():
+    tp = TProfile('tp', 'JES Profile;jetpt;JES', 20, 100, 500)
     i=0
     max = tr.GetEntriesFast()
     while i<max:
@@ -77,7 +175,7 @@ def DrawJESDiff():
     tl.SetLineColor(2)
     tl.SetLineWidth(2)
     tl.Draw("same")
-    c.SaveAs(prefix+'_jes_diff_'+suffix+'.pdf')
+    c.SaveAs(prefix+'_jes_profile_'+suffix+'.pdf')
 
 def Draw_z():
     h1 = TH1F('h1', 'zz', 50, 0, 0.5)
@@ -107,6 +205,7 @@ def Draw_z():
     p1.Draw()
     p1.cd()
     fr1 = p1.DrawFrame(-0.01, 6.5, 0.51, 2.1e2,"")
+    # fr1 = p1.DrawFrame(-0.01, 0.5, 0.51, 2.1e1,"")
 
     fr1.GetXaxis().SetTitleSize(0.15/3)
     fr1.GetXaxis().SetLabelSize(0.15/3)
@@ -127,7 +226,7 @@ def Draw_z():
     h1.Draw("same")
     h2.Draw("same")
     lg = TLegend(0.6, 0.7, 0.88, 0.88)
-    lg.AddEntry(h1, 'Jet', 'le')
+    lg.AddEntry(h1, 'jet', 'le')
     lg.AddEntry(h2, prefix, 'le')
     lg.Draw('same')
 
@@ -157,7 +256,8 @@ def Draw_z():
     fr2.GetYaxis().SetLabelSize(0.14)
     fr2.GetYaxis().SetTitleSize(0.14)
     fr2.GetYaxis().SetTitleOffset(0.4)
-    fr2.GetYaxis().SetTitle("#frac{icsJet}{Jet}")
+    title_y = '#frac{' + tree + '}{jet}'
+    fr2.GetYaxis().SetTitle(title_y)
     fr2.GetYaxis().CenterTitle()
 
     h3 = h2.Clone()
@@ -200,7 +300,7 @@ def Draw_delta():
     p1.SetLeftMargin(0.15)
     p1.Draw()
     p1.cd()
-    fr1 = p1.DrawFrame(-0.01, 2., 0.41, 3e2,"")
+    fr1 = p1.DrawFrame(-0.01, 2e-1, 0.41, 3e2,"")
 
     fr1.GetXaxis().SetTitleSize(0.15/3)
     fr1.GetXaxis().SetLabelSize(0.15/3)
@@ -221,7 +321,7 @@ def Draw_delta():
     h1.Draw("same")
     h2.Draw("same")
     lg = TLegend(0.6, 0.7, 0.88, 0.88)
-    lg.AddEntry(h1, 'Jet', 'le')
+    lg.AddEntry(h1, 'jet', 'le')
     lg.AddEntry(h2, prefix, 'le')
     lg.Draw('same')
 
@@ -252,7 +352,8 @@ def Draw_delta():
     fr2.GetYaxis().SetLabelSize(0.12)
     fr2.GetYaxis().SetTitleSize(0.12)
     fr2.GetYaxis().SetTitleOffset(0.4)
-    fr2.GetYaxis().SetTitle("#frac{icsJet}{Jet}")
+    title_y = '#frac{' + tree + '}{jet}'
+    fr2.GetYaxis().SetTitle(title_y)
     fr2.GetYaxis().CenterTitle()
 
     h3 = h2.Clone()
@@ -310,7 +411,7 @@ def Draw_depth():
     h1.Draw("same")
     h2.Draw("same")
     lg = TLegend(0.6, 0.7, 0.88, 0.88)
-    lg.AddEntry(h1, 'Jet', 'le')
+    lg.AddEntry(h1, 'jet', 'le')
     lg.AddEntry(h2, prefix, 'le')
     lg.Draw('same')
 
@@ -334,7 +435,8 @@ def Draw_depth():
     fr2.GetYaxis().SetLabelSize(0.14)
     fr2.GetYaxis().SetTitleSize(0.14)
     fr2.GetYaxis().SetTitleOffset(0.4)
-    fr2.GetYaxis().SetTitle("#frac{icsJet}{Jet}")
+    title_y = '#frac{' + tree + '}{jet}'
+    fr2.GetYaxis().SetTitle(title_y)
     fr2.GetYaxis().CenterTitle()
 
     h3 = h2.Clone()
@@ -342,7 +444,7 @@ def Draw_depth():
     h3.Divide(h1)
     h3.Draw("same")
 
-    tl = TLine(-0.01, 1, 12.1, 1)
+    tl = TLine(-0.01, 1, 20.1, 1)
     tl.SetLineStyle(7)
     # tl.SetLineColor(2)
     tl.SetLineWidth(1)
@@ -350,8 +452,178 @@ def Draw_depth():
 
     c.SaveAs(prefix+'_depth_'+suffix+'.pdf')
 
-DrawJES()
+def Draw_depthg():
+    h1 = TH1F('h1', 'depth', 20, 0., 20.)
+    h2 = TH1F('h2', 'depth', 20, 0., 20.)
+    i=0
+    bin_size = 20./20.
+    njet=tr.GetEntriesFast()
+    while i<njet:
+        tr.GetEntry(i)
+        if tr.depthg==0:
+            i+=1
+            continue
+        h1.Fill(tr.depthg)
+        h2.Fill(tr.depthgm)
+        i+=1
+    c = TCanvas('c', 'c', 600, 600)
+    gStyle.SetOptStat(0000)
+
+    p1 = TPad("p1", "p1", 0, 0.25, 1., 1.)
+    p1.SetBottomMargin(0)
+    p1.SetLeftMargin(0.15)
+    p1.Draw()
+    p1.cd()
+    fr1 = p1.DrawFrame(-0.01, 0, 20.1, 0.3,"")
+    fr1.GetXaxis().SetTitleSize(0.15/3)
+    fr1.GetXaxis().SetLabelSize(0.15/3)
+
+
+    fr1.GetYaxis().SetTitleSize(0.14/3)
+    fr1.GetYaxis().SetTitleOffset(1.2)
+    fr1.GetYaxis().SetLabelSize(0.14/3)
+    fr1.GetYaxis().SetTitle("#frac{1}{N_{jet}} #frac{dN}{dz}")
+    fr1.GetYaxis().CenterTitle()
+
+    h1.Scale(1./njet/bin_size)
+    h2.Scale(1./njet/bin_size)
+    h1.SetLineColor(4)
+    h1.SetLineWidth(2)
+    h2.SetLineColor(2)
+    h2.SetLineWidth(2)
+    h1.Draw("same")
+    h2.Draw("same")
+    lg = TLegend(0.6, 0.7, 0.88, 0.88)
+    lg.AddEntry(h1, 'jet', 'le')
+    lg.AddEntry(h2, prefix, 'le')
+    lg.Draw('same')
+
+    c.cd()
+    p2 = TPad("p2", "p2", 0, 0, 1., 0.25)
+    p2.SetTopMargin(0)
+    p2.SetLeftMargin(0.15)
+    p2.SetBottomMargin(0.3)
+    # p2.SetGridy()
+    p2.Draw()
+    p2.cd()
+
+    fr2 = p2.DrawFrame(-0.01,0.2,20.1,2.,"")
+
+    fr2.GetXaxis().SetLabelSize(0.15)
+    fr2.GetXaxis().SetTitleSize(0.15)
+    fr2.GetXaxis().SetTitle("depthg")
+    fr2.GetXaxis().CenterTitle()
+
+    fr2.GetYaxis().SetNdivisions(8)
+    fr2.GetYaxis().SetLabelSize(0.14)
+    fr2.GetYaxis().SetTitleSize(0.14)
+    fr2.GetYaxis().SetTitleOffset(0.4)
+    title_y = '#frac{' + tree + '}{jet}'
+    fr2.GetYaxis().SetTitle(title_y)
+    fr2.GetYaxis().CenterTitle()
+
+    h3 = h2.Clone()
+    h3.Sumw2()
+    h3.Divide(h1)
+    h3.Draw("same")
+
+    tl = TLine(-0.01, 1, 20.1, 1)
+    tl.SetLineStyle(7)
+    # tl.SetLineColor(2)
+    tl.SetLineWidth(1)
+    tl.Draw("same")
+
+    c.SaveAs(prefix+'_depthg_'+suffix+'.pdf')
+
+def Draw_constituent_pt():
+    h1 = TH1F('h1', '', 200, 0, 50)
+    h2 = TH1F('h2', '', 200, 0, 50)
+    i=0
+    bin_size = 50./200
+    njet=tr.GetEntriesFast()
+    while i<njet:
+        tr.GetEntry(i)
+        for id in range(tr.cn):
+            h1.Fill(tr.cpt[id])
+        for id in range(tr.cnm):
+            h2.Fill(tr.cptm[id])
+        i+=1
+    c = TCanvas('c', 'c', 600, 600)
+    gStyle.SetOptStat(0000)
+
+    p1 = TPad("p1", "p1", 0, 0.25, 1., 1.)
+    p1.SetLogy(1)
+    p1.SetBottomMargin(0)
+    p1.SetLeftMargin(0.15)
+    p1.Draw()
+    p1.cd()
+    fr1 = p1.DrawFrame(-0.1, 2e-5, 20.1, 3e5,"")
+
+    fr1.GetXaxis().SetTitleSize(0.15/3)
+    fr1.GetXaxis().SetLabelSize(0.15/3)
+
+    fr1.GetYaxis().SetTitleSize(0.14/3)
+    fr1.GetYaxis().SetTitleOffset(1.2)
+    fr1.GetYaxis().SetLabelSize(0.14/3)
+    fr1.GetYaxis().SetTitle("#frac{1}{N_{jet}} #frac{dN}{dcpt}")
+    fr1.GetYaxis().CenterTitle()
+
+    h1.Scale(1./njet/bin_size)
+    h2.Scale(1./njet/bin_size)
+    h1.SetLineColor(4)
+    h1.SetLineWidth(2)
+    h2.SetLineColor(2)
+    h2.SetLineWidth(2)
+    h1.Draw("same")
+    h2.Draw("same")
+    lg = TLegend(0.6, 0.7, 0.88, 0.88)
+    lg.AddEntry(h1, 'jet', 'le')
+    lg.AddEntry(h2, prefix, 'le')
+    lg.Draw('same')
+
+    c.cd()
+    p2 = TPad("p2", "p2", 0, 0, 1., 0.25)
+    p2.SetTopMargin(0)
+    p2.SetLeftMargin(0.15)
+    p2.SetBottomMargin(0.3)
+
+    # p2.SetGridy()
+    p2.Draw()
+    p2.cd()
+
+    fr2 = p2.DrawFrame(-0.1,0.42,20.1,1.24,"")
+
+    fr2.GetXaxis().SetLabelSize(0.15)
+    fr2.GetXaxis().SetTitleSize(0.15)
+    fr2.GetXaxis().SetTitle("cpt")
+    fr2.GetXaxis().CenterTitle()
+
+    fr2.GetYaxis().SetNdivisions(8)
+    fr2.GetYaxis().SetLabelSize(0.12)
+    fr2.GetYaxis().SetTitleSize(0.12)
+    fr2.GetYaxis().SetTitleOffset(0.4)
+    title_y = '#frac{' + tree + '}{jet}'
+    fr2.GetYaxis().SetTitle(title_y)
+    fr2.GetYaxis().CenterTitle()
+
+    h3 = h2.Clone()
+    h3.Sumw2()
+    h3.Divide(h1)
+    h3.Draw("same")
+
+    tl = TLine(-0.1, 1, 20.1, 1)
+    tl.SetLineStyle(7)
+    # tl.SetLineColor(2)
+    tl.SetLineWidth(1)
+    tl.Draw("same")
+
+    c.SaveAs(prefix+'_cpt_'+suffix+'.pdf')
+
+# DrawJES()
+# DrawJESProfile()
 DrawJESDiff()
 Draw_z()
 Draw_delta()
-Draw_depth()
+# Draw_depth()
+# Draw_depthg()
+Draw_constituent_pt()
--- analysis/resolution.py
+++ analysis/resolution.py
@@ -9,37 +9,38 @@ import sys
 
 
 try:
-    input = sys.argv[1]
-    output = sys.argv[2]
+    target = sys.argv[1]
+    inputs = sys.argv[2:]
 except:
-    input = '../default.root'
-    output = './resolution.root'
+    target = './resolution.root'
+    inputs = ['../default.root']
 
-print('input file: %s' % input)
-print('output file: %s' % output)
-
-f = TFile(input, 'READ')
-tr = f.Get('jet')
-tr_cs = f.Get('csjet')
-tr_ics = f.Get('icsjet')
+print('target: %s' % target)
+print('inputs:' + ' '.join(inputs))
 
 #output
-of = TFile(output, 'recreate')
-otr_cs = TTree('csjet', 'csjet')
+of = TFile(target, 'recreate')
+otr_csj = TTree('csjjet', 'csjjet')
+otr_cse = TTree('csejet', 'csejet')
 otr_ics = TTree('icsjet', 'icsjet')
 
 jetpt = array('f', [0.])
-jetptcs = array('f', [0.])
+jetptm = array('f', [0.])
 
+jetm = array('f', [0.])
+jetmm = array('f', [0.])
+
+jetmg = array('f', [0.])
+jetmgm = array('f', [0.])
 
 scale = array('f', [0.])
-# res = array('f', [0.])
 
 deta = array('f', [0.])
 dphi = array('f', [0.])
 
 # sub structure
 depth = array('i', [0])
+depthg = array('i', [0])
 z = array('f', 40*[0.])
 delta = array('f', 40*[0.])
 kperp = array('f', 40*[0.])
@@ -47,59 +48,116 @@ m = array('f', 40*[0.])
 
 # matched sub structure
 depthm = array('i', [0])
+depthgm = array('i', [0])
 zm = array('f', 40*[0.])
 deltam = array('f', 40*[0.])
 kperpm = array('f', 40*[0.])
 mm = array('f', 40*[0.])
 
-# sub structure matched
+# constituents
+cn = array('i', [0])
+cpt = array('f', 500 * [0.])
+cdeta = array('f', 500 * [0.])
+cdphi = array('f', 500 * [0.])
+cnm = array('i', [0])
+cptm = array('f', 500 * [0.])
+cdetam = array('f', 500 * [0.])
+cdphim = array('f', 500 * [0.])
 
 # branches
-otr_cs.Branch('jetpt', jetpt, 'jetpt/F')
-otr_cs.Branch('jetptcs', jetptcs, 'jetptcs/F')
-otr_cs.Branch('scale', scale, 'scale/F')
-# otr_cs.Branch('jer', jer, 'jer/F')
-otr_cs.Branch('deta', deta, 'deta/F')
-otr_cs.Branch('dphi', dphi, 'dphi/F')
+otr_csj.Branch('jetpt', jetpt, 'jetpt/F')
+otr_csj.Branch('jetptm', jetptm, 'jetptm/F')
+otr_csj.Branch('jetm', jetm, 'jetm/F')
+otr_csj.Branch('jetmm', jetmm, 'jetmm/F')
+otr_csj.Branch('jetmg', jetmg, 'jetmg/F')
+otr_csj.Branch('jetmgm', jetmgm, 'jetmgm/F')
+otr_csj.Branch('scale', scale, 'scale/F')
+otr_csj.Branch('deta', deta, 'deta/F')
+otr_csj.Branch('dphi', dphi, 'dphi/F')
+otr_csj.Branch('depth', depth, 'depth/I')
+otr_csj.Branch('depthg', depthg, 'depthg/I')
+otr_csj.Branch('z', z, 'z[depth]/F')
+otr_csj.Branch('delta', delta, 'delta[depth]/F')
+otr_csj.Branch('kperp', kperp, 'kperp[depth]/F')
+otr_csj.Branch('m', m, 'm[depth]/F')
+otr_csj.Branch('depthm', depthm, 'depthm/I')
+otr_csj.Branch('depthgm', depthgm, 'depthgm/I')
+otr_csj.Branch('zm', zm, 'zm[depthm]/F')
+otr_csj.Branch('deltam', deltam, 'deltam[depthm]/F')
+otr_csj.Branch('kperpm', kperpm, 'kperpm[depthm]/F')
+otr_csj.Branch('mm', m, 'mm[depthm]/F')
+# constituents
+otr_csj.Branch('cn', cn, 'cn/I')
+otr_csj.Branch('cpt', cpt, 'cpt[cn]/F')
+otr_csj.Branch('cdeta', cdeta, 'cdeta[cn]/F')
+otr_csj.Branch('cdphi', cdphi, 'cdphi[cn]/F')
+otr_csj.Branch('cnm', cnm, 'cnm/I')
+otr_csj.Branch('cptm', cptm, 'cptm[cnm]/F')
+otr_csj.Branch('cdetam', cdetam, 'cdetam[cn]/F')
+otr_csj.Branch('cdphim', cdphim, 'cdphim[cn]/F')
 
-otr_cs.Branch('depth', depth, 'depth/I')
-otr_cs.Branch('z', z, 'z[depth]/F')
-otr_cs.Branch('delta', delta, 'delta[depth]/F')
-otr_cs.Branch('kperp', kperp, 'kperp[depth]/F')
-otr_cs.Branch('m', m, 'm[depth]/F')
-
-otr_cs.Branch('depthm', depthm, 'depthm/I')
-otr_cs.Branch('zm', zm, 'zm[depthm]/F')
-otr_cs.Branch('deltam', deltam, 'deltam[depthm]/F')
-otr_cs.Branch('kperpm', kperpm, 'kperpm[depthm]/F')
-otr_cs.Branch('mm', m, 'mm[depthm]/F')
+otr_cse.Branch('jetpt', jetpt, 'jetpt/F')
+otr_cse.Branch('jetptm', jetptm, 'jetptm/F')
+otr_cse.Branch('jetm', jetm, 'jetm/F')
+otr_cse.Branch('jetmm', jetmm, 'jetmm/F')
+otr_cse.Branch('jetmg', jetmg, 'jetmg/F')
+otr_cse.Branch('jetmgm', jetmgm, 'jetmgm/F')
+otr_cse.Branch('scale', scale, 'scale/F')
+otr_cse.Branch('deta', deta, 'deta/F')
+otr_cse.Branch('dphi', dphi, 'dphi/F')
+otr_cse.Branch('depth', depth, 'depth/I')
+otr_cse.Branch('depthg', depthg, 'depthg/I')
+otr_cse.Branch('z', z, 'z[depth]/F')
+otr_cse.Branch('delta', delta, 'delta[depth]/F')
+otr_cse.Branch('kperp', kperp, 'kperp[depth]/F')
+otr_cse.Branch('m', m, 'm[depth]/F')
+otr_cse.Branch('depthm', depthm, 'depthm/I')
+otr_cse.Branch('depthgm', depthgm, 'depthgm/I')
+otr_cse.Branch('zm', zm, 'zm[depthm]/F')
+otr_cse.Branch('deltam', deltam, 'deltam[depthm]/F')
+otr_cse.Branch('kperpm', kperpm, 'kperpm[depthm]/F')
+otr_cse.Branch('mm', m, 'mm[depthm]/F')
+# constituents
+otr_cse.Branch('cn', cn, 'cn/I')
+otr_cse.Branch('cpt', cpt, 'cpt[cn]/F')
+otr_cse.Branch('cdeta', cdeta, 'cdeta[cn]/F')
+otr_cse.Branch('cdphi', cdphi, 'cdphi[cn]/F')
+otr_cse.Branch('cnm', cnm, 'cnm/I')
+otr_cse.Branch('cptm', cptm, 'cptm[cnm]/F')
+otr_cse.Branch('cdetam', cdetam, 'cdetam[cn]/F')
+otr_cse.Branch('cdphim', cdphim, 'cdphim[cn]/F')
 
 otr_ics.Branch('jetpt', jetpt, 'jetpt/F')
-otr_ics.Branch('jetptcs', jetptcs, 'jetptcs/F')
+otr_ics.Branch('jetptm', jetptm, 'jetptm/F')
+otr_ics.Branch('jetm', jetm, 'jetm/F')
+otr_ics.Branch('jetmm', jetmm, 'jetmm/F')
+otr_ics.Branch('jetmg', jetmg, 'jetmg/F')
+otr_ics.Branch('jetmgm', jetmgm, 'jetmgm/F')
 otr_ics.Branch('scale', scale, 'scale/F')
-# otr_ics.Branch('jer', jer, 'jer/F')
 otr_ics.Branch('deta', deta, 'deta/F')
 otr_ics.Branch('dphi', dphi, 'dphi/F')
-
 otr_ics.Branch('depth', depth, 'depth/I')
+otr_ics.Branch('depthg', depthg, 'depthg/I')
 otr_ics.Branch('z', z, 'z[depth]/F')
 otr_ics.Branch('delta', delta, 'delta[depth]/F')
 otr_ics.Branch('kperp', kperp, 'kperp[depth]/F')
 otr_ics.Branch('m', m, 'm[depth]/F')
-
 otr_ics.Branch('depthm', depthm, 'depthm/I')
+otr_ics.Branch('depthgm', depthgm, 'depthgm/I')
 otr_ics.Branch('zm', zm, 'zm[depthm]/F')
 otr_ics.Branch('deltam', deltam, 'deltam[depthm]/F')
 otr_ics.Branch('kperpm', kperpm, 'kperpm[depthm]/F')
 otr_ics.Branch('mm', m, 'mm[depthm]/F')
+# constituents
+otr_ics.Branch('cn', cn, 'cn/I')
+otr_ics.Branch('cpt', cpt, 'cpt[cn]/F')
+otr_ics.Branch('cdeta', cdeta, 'cdeta[cn]/F')
+otr_ics.Branch('cdphi', cdphi, 'cdphi[cn]/F')
+otr_ics.Branch('cnm', cnm, 'cnm/I')
+otr_ics.Branch('cptm', cptm, 'cptm[cnm]/F')
+otr_ics.Branch('cdetam', cdetam, 'cdetam[cn]/F')
+otr_ics.Branch('cdphim', cdphim, 'cdphim[cn]/F')
 
-
-#matching
-idx = 0
-idx_cs = 0
-idx_ics = 0
-max = tr.GetEntriesFast()
-# max = 10
 
 def delta_phi(phi1, phi2):
     pi = 3.1415926
@@ -135,70 +193,161 @@ def match(tr, tr_match, idx, idx_matched=0):
         idx_matched += 1
     return -999
 
-while idx<max:
-    # cs matching
+for input in inputs:
+    f = TFile(input, 'READ')
+    tr = f.Get('jet')
+    tr_csj = f.Get('csjjet')
+    tr_cse = f.Get('csejet')
+    tr_ics = f.Get('icsjet')
 
-    temp = match(tr, tr_cs, idx, idx_cs)
-    if temp>=0:
-        idx_cs = temp
-        # print('%d/%d'%(idx, idx_cs))
+    #matching
+    idx = 0
+    idx_csj = 0
+    idx_cse = 0
+    idx_ics = 0
+    max = tr.GetEntriesFast()
+    # max = 10
 
-        tr.GetEntry(idx)
-        tr_cs.GetEntry(idx_cs)
-        jetpt[0] = tr.jetpt
-        jetptcs[0] = tr_cs.jetpt
-        deta[0] = tr_cs.eta - tr.eta
-        dphi[0] = tr_cs.phi - tr.phi
-        scale[0] = jetptcs[0]/jetpt[0]
+    while idx<max:
+        # csj matching
+        temp = match(tr, tr_csj, idx, idx_csj)
+        if temp>=0:
+            idx_csj = temp
+            tr.GetEntry(idx)
+            tr_csj.GetEntry(idx_csj)
+            jetpt[0] = tr.jetpt
+            jetptm[0] = tr_csj.jetpt
+            jetm[0] = tr.jetm
+            jetmm[0] = tr_csj.jetm
+            jetmg[0] = tr.jetmg
+            jetmgm[0] = tr_csj.jetmg
+            deta[0] = tr_csj.eta - tr.eta
+            dphi[0] = delta_phi(tr_csj.phi, tr.phi)
+            scale[0] = jetptm[0]/jetpt[0]
+            depth[0] = tr.depth
+            depthm[0] = tr_csj.depth
+            depthg[0] = tr.depthg
+            depthgm[0] = tr_csj.depthg
+            cn[0] = tr.cn
+            cnm[0] = tr_csj.cn
 
-        depth[0] = tr.depth
-        # print(tr.depth)
-        for i in range(depth[0]):
-            z[i] = tr.z[i]
-            delta[i] = tr.delta[i]
-            kperp[i] = tr.kperp[i]
-            m[i] = tr.m[i]
+            for i in range(depth[0]):
+                z[i] = tr.z[i]
+                delta[i] = tr.delta[i]
+                kperp[i] = tr.kperp[i]
+                m[i] = tr.m[i]
 
-        depthm[0] = tr_cs.depth
-        for i in range(depthm[0]):
-            zm[i] = tr_cs.z[i]
-            deltam[i] = tr_cs.delta[i]
-            kperpm[i] = tr_cs.kperp[i]
-            mm[i] = tr_cs.m[i]
+            for i in range(depthm[0]):
+                zm[i] = tr_csj.z[i]
+                deltam[i] = tr_csj.delta[i]
+                kperpm[i] = tr_csj.kperp[i]
+                mm[i] = tr_csj.m[i]
 
-        otr_cs.Fill()
+            for i in range(cn[0]):
+                cpt[i] = tr.cpt[i]
+                cdeta[i] = tr.cdeta[i]
+                cdphi[i] = tr.cdphi[i]
 
-    # ics matching
-    temp = match(tr, tr_ics, idx, idx_ics)
-    if temp>=0:
-        idx_ics = temp
-        # print('%d/%d'%(idx, idx_ics))
+            for i in range(cnm[0]):
+                cptm[i] = tr_csj.cpt[i]
+                cdetam[i] = tr_csj.cdeta[i]
+                cdphim[i] = tr_csj.cdphi[i]
 
-        tr.GetEntry(idx)
-        tr_ics.GetEntry(idx_ics)
-        jetpt[0] = tr.jetpt
-        jetptcs[0] = tr_ics.jetpt
-        deta[0] = tr_ics.eta - tr.eta
-        dphi[0] = tr_ics.phi - tr.phi
-        scale[0] = jetptcs[0]/jetpt[0]
+            otr_csj.Fill()
 
-        depth[0] = tr.depth
-        for i in range(depth[0]):
-            z[i] = tr.z[i]
-            delta[i] = tr.delta[i]
-            kperp[i] = tr.kperp[i]
-            m[i] = tr.m[i]
+        # cse matching
+        temp = match(tr, tr_cse, idx, idx_cse)
+        if temp>=0:
+            idx_cse = temp
+            tr.GetEntry(idx)
+            tr_cse.GetEntry(idx_cse)
+            jetpt[0] = tr.jetpt
+            jetptm[0] = tr_cse.jetpt
+            jetm[0] = tr.jetm
+            jetmm[0] = tr_cse.jetm
+            jetmg[0] = tr.jetmg
+            jetmgm[0] = tr_cse.jetmg
+            deta[0] = tr_cse.eta - tr.eta
+            dphi[0] = delta_phi(tr_cse.phi, tr.phi)
+            scale[0] = jetptm[0]/jetpt[0]
+            depth[0] = tr.depth
+            depthm[0] = tr_cse.depth
+            depthg[0] = tr.depthg
+            depthgm[0] = tr_cse.depthg
+            cn[0] = tr.cn
+            cnm[0] = tr_cse.cn
 
-        depthm[0] = tr_ics.depth
-        for i in range(depthm[0]):
-            zm[i] = tr_ics.z[i]
-            deltam[i] = tr_ics.delta[i]
-            kperpm[i] = tr_ics.kperp[i]
-            mm[i] = tr_ics.m[i]
+            for i in range(depth[0]):
+                z[i] = tr.z[i]
+                delta[i] = tr.delta[i]
+                kperp[i] = tr.kperp[i]
+                m[i] = tr.m[i]
 
-        otr_ics.Fill()
+            for i in range(depthm[0]):
+                zm[i] = tr_cse.z[i]
+                deltam[i] = tr_cse.delta[i]
+                kperpm[i] = tr_cse.kperp[i]
+                mm[i] = tr_cse.m[i]
 
-    idx+=1
+            for i in range(cn[0]):
+                cpt[i] = tr.cpt[i]
+                cdeta[i] = tr.cdeta[i]
+                cdphi[i] = tr.cdphi[i]
+
+            for i in range(cnm[0]):
+                cptm[i] = tr_cse.cpt[i]
+                cdetam[i] = tr_cse.cdeta[i]
+                cdphim[i] = tr_cse.cdphi[i]
+
+            otr_cse.Fill()
+
+        # ics matching
+        temp = match(tr, tr_ics, idx, idx_ics)
+        if temp>=0:
+            idx_ics = temp
+            tr.GetEntry(idx)
+            tr_ics.GetEntry(idx_ics)
+            jetpt[0] = tr.jetpt
+            jetptm[0] = tr_ics.jetpt
+            jetm[0] = tr.jetm
+            jetmm[0] = tr_cse.jetm
+            jetmg[0] = tr.jetmg
+            jetmgm[0] = tr_cse.jetmg
+            deta[0] = tr_ics.eta - tr.eta
+            dphi[0] = delta_phi(tr_ics.phi, tr.phi)
+            scale[0] = jetptm[0]/jetpt[0]
+            depth[0] = tr.depth
+            depthm[0] = tr_ics.depth
+            depthg[0] = tr.depthg
+            depthgm[0] = tr_ics.depthg
+            cn[0] = tr.cn
+            cnm[0] = tr_ics.cn
+
+            for i in range(depth[0]):
+                z[i] = tr.z[i]
+                delta[i] = tr.delta[i]
+                kperp[i] = tr.kperp[i]
+                m[i] = tr.m[i]
+
+            for i in range(depthm[0]):
+                zm[i] = tr_ics.z[i]
+                deltam[i] = tr_ics.delta[i]
+                kperpm[i] = tr_ics.kperp[i]
+                mm[i] = tr_ics.m[i]
+
+            for i in range(cn[0]):
+                cpt[i] = tr.cpt[i]
+                cdeta[i] = tr.cdeta[i]
+                cdphi[i] = tr.cdphi[i]
+
+            for i in range(cnm[0]):
+                cptm[i] = tr_ics.cpt[i]
+                cdetam[i] = tr_ics.cdeta[i]
+                cdphim[i] = tr_ics.cdphi[i]
+
+            otr_ics.Fill()
+
+        idx+=1
 
 of.Write()
 of.Close()
